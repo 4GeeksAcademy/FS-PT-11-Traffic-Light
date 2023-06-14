fd90edf5b107947b8d71087af51617bada9efaf1
Adriano Schneider Maximiano <adriano-sm@hotmail.com>
2023-06-15 00:34:28 +0100

    Creating a trafick light on web
    React page with a trafic light that change colors every 3 seconds and also when you click on the light.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+//create your first component.
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  let color = "";
+
+  window.onload = function () {
+    changeRed;
+  };
+
+  useEffect(() => {
+    const changeRed = () => {
+      setSelectedColor("red");
+      setTimeout(() => {
+        changeYellow();
+      }, 3000);
+    };
+
+    const changeYellow = () => {
+      setSelectedColor("yellow");
+      setTimeout(() => {
+        changeGreen();
+      }, 3000);
+    };
+
+    const changeGreen = () => {
+      setSelectedColor("green");
+      setTimeout(() => {
+        changeRed();
+      }, 3000);
+    };
+
+    setTimeout(() => {
+      changeRed();
+    }, 3000);
+  }, []);
+
+  const [selectedColor, setSelectedColor] = useState(color);
+
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5">Trafic Light</h1>
+      <p>This trafic light change the color every 3 seconds</p>
+      <div className="traficLight">
+        <div
+          onClick={() => setSelectedColor("red")}
+          className={
+            "red" + (selectedColor === "red" ? " glowRed" : " opacity")
+          }
+        ></div>
+        <div
+          onClick={() => setSelectedColor("yellow")}
+          className={
+            "yellow" + (selectedColor === "yellow" ? " glowYellow" : " opacity")
+          }
+        ></div>
+        <div
+          onClick={() => setSelectedColor("green")}
+          className={
+            "green" + (selectedColor === "green" ? " glowGreen" : " opacity")
+          }
+        ></div>
+      </div>
+      <div className="mt-5">
+        <button onClick={document.querySelector} className="butn">
+          Add purple color
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
